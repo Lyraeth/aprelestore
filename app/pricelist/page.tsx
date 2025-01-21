a0a13c6eca965d1ff3fe44aa416629761998961e
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2025-01-21 18:13:40 +0700

    create and edit: create Pricelist page and Priceliste components
    - success fetch data from Supabase to Pricelist page, but still need improving on UI

---
+++ app/pricelist/page.tsx
@@ -0,0 +1,53 @@
+import { Playwrite_AR, Montserrat } from "next/font/google";
+import { createClient } from "@/utils/supabase/server";
+import Pricelists from "../components/Pricelists";
+
+const playwrite_ar = Playwrite_AR({
+    weight: "400",
+});
+
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+export default async function Pricelist() {
+    const supabase = await createClient();
+
+    const { data: applications, error } = await supabase.from("application")
+        .select(`
+            id,
+            name,
+            created_at,
+            services:service (
+                id,
+                duration,
+                price,
+                account_type,
+                description,
+                created_at
+            )
+        `);
+
+    if (error) {
+        console.error("Error fetching data:", error);
+        return <div className="text-red-500">Error fetching data</div>;
+    }
+
+    return (
+        <div className="min-h-screen p-8 sm:p-20">
+            <header className="mb-12 text-center my-5">
+                <div className={playwrite_ar.className}>
+                    <h1 className="text-6xl font-bold mb-10">Aprelestore</h1>
+                </div>
+                <div className={montserrat.className}>
+                    <p className="mt-2 text-lg">
+                        Explore our services and find the best option for you.
+                    </p>
+                </div>
+            </header>
+            <div className="border-b"></div>
+            {/* <Pricelists /> */}
+        </div>
+    );
+}
