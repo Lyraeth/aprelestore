6e3f387b87a7b590c985920034c93d1fbb9f4479
Mahsa Nurfarhan Hidayat <nurfarhan52@gmail.com>
2025-01-24 09:34:22 +0700

    feat: create UI for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 import { Playwrite_AR } from "next/font/google";
-
+import Link from "next/link";
 const playwrite_ar = Playwrite_AR({
     weight: "400",
 });
@@ -8,9 +8,44 @@ export default function Contact() {
     return (
         <div className="bg-gradient-to-b from-pink-200/80 via-pink-300/80 to-pink-400/80 dark:bg-gradient-to-b dark:from-black dark:via-gray-900 dark:to-gray-700">
             <div className={playwrite_ar.className}>
-                <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-                    <h1> Will be added soon!</h1>
-                </div>
+                <main className="min-h-screen p-8 sm:p-20">
+                    <h1 className="mb-6 text-center text-2xl md:text-4xl xl:text-6xl my-5">
+                        {" "}
+                        My Contact
+                    </h1>
+                    <div className="grid grid-cols-2 xl:grid-cols-3 w-full text-center my-20 gap-4">
+                        <Link
+                            href={`https://t.me/Apreleisa`}
+                            target="_blank"
+                            className="py-10 bg-blue-300 rounded-lg border-blue-200 bg-gradient-to-b from-pink-200/80 via-pink-300/80 to-pink-400/80 dark:bg-gradient-to-b dark:from-gray-700 dark:to-gray-800 hover:bg-blue-300 dark:hover:to-gray-900 hover:translate-y-1"
+                        >
+                            <div className="mb-10 font-[family-name:var(--font-geist-sans)]">
+                                Telegram
+                            </div>
+                            <div>Apreleisa</div>
+                        </Link>
+                        <Link
+                            href={`https://twitter.com/wfrcoklat`}
+                            target="_blank"
+                            className="py-10 bg-blue-300 rounded-lg border-blue-200 bg-gradient-to-b from-pink-200/80 via-pink-300/80 to-pink-400/80 dark:bg-gradient-to-b dark:from-gray-700 dark:to-gray-800 hover:bg-blue-300 dark:hover:to-gray-900 hover:translate-y-1 "
+                        >
+                            <div className="mb-10 font-[family-name:var(--font-geist-sans)]">
+                                X / Twitter
+                            </div>
+                            <div>@wfrcoklat</div>
+                        </Link>
+                        <Link
+                            href={`https://wa.me/628993337395`}
+                            target="_blank"
+                            className="py-10 bg-blue-300 rounded-lg border-blue-200 bg-gradient-to-b from-pink-200/80 via-pink-300/80 to-pink-400/80 dark:bg-gradient-to-b dark:from-gray-700 dark:to-gray-800 hover:bg-blue-300 dark:hover:to-gray-900 hover:translate-y-1 col-span-2 xl:col-span-1"
+                        >
+                            <div className="mb-10 font-[family-name:var(--font-geist-sans)]">
+                                WhatsApp
+                            </div>
+                            <div>lele</div>
+                        </Link>
+                    </div>
+                </main>
             </div>
         </div>
     );
